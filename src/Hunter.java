59cbba47d2382cfccacbcfd375ae202b7aee5519
BT_1N3_14 <sample@bths.edu>
2025-01-14 13:17:20 -0500

    finished task 4 (added new terrain and item)

--- src/Hunter.java
+++ src/Hunter.java
@@ -115,7 +115,7 @@ public class Hunter {
      */
     public boolean hasItemInKit(String item) {
         for (String tmpItem : kit) {
-            if (item.equals(tmpItem)) {
+            if (item.equalsIgnoreCase(tmpItem)) {
                 // early return
                 return true;
             }
